bf65a081048a3e2fb2c974bad5973a00edce3586
vathdev <vthien562004@gmail.com>
2025-09-24 17:11:22 +0700

    Edit docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Vath Dev',
+  title: 'My van thien',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
